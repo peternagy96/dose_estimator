e807ec4003589d7ed351b75260bcb0d04f780f99
Peter Nagy <peternagy96@gmail.com>
2019-04-08 14:37:46 +0200

    added data loader file with first test and changed its init

---
+++ dose_estimator/data_loader/data_loader.py
@@ -0,0 +1,5 @@
+class dataLoader:
+
+    def __init__(self):
+        self.input = 0
+        self.output = 0
---
+++ dose_estimator/tests/test_data_loader.py
@@ -0,0 +1,5 @@
+from data_loader.data_loader import dataLoader
+g
+def test_data_loader():
+    dl = dataLoader()
+    assert 0 == 0
