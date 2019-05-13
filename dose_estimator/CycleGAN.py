data normalization fixed: data is between -1 and 1

dose_estimator/CycleGAN.py
@@ -1,5 +1,7 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ["PATH"] = "/usr/local/cuda-9.0/bin${PATH:+:${PATH}}"
+os.environ["LD_LIBRARY_PATH"] = "/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}"
 from keras.layers import Layer, Input, Conv2D, Activation, add, BatchNormalization, UpSampling2D, ZeroPadding2D, Conv2DTranspose, Flatten, MaxPooling2D, AveragePooling2D
 #from keras.utils.conv_utils import normalize_data_format
 from keras_contrib.layers.normalization.instancenormalization import InstanceNormalization, InputSpec
@@ -22,16 +24,21 @@ import math
 import csv
 import sys
 #import os
+from PIL import Image
 
 import keras.backend as K
 import tensorflow as tf
 import load_data
-os.environ["CUDA_VISIBLE_DEVICES"]="1" 
+os.environ["CUDA_VISIBLE_DEVICES"]="2"
 np.random.seed(seed=12345)
 
 
+# export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
+# export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+
 class CycleGAN():
-    def __init__(self, lr_D=2e-4, lr_G=3e-4, image_shape=(240, 240, 1),
+    def __init__(self, lr_D=2e-4, lr_G=3e-4, image_shape=(200, 200, 1),
                  date_time_string_addition='', image_folder='MR'):
         self.img_shape = image_shape
         self.channels = self.img_shape[-1]
@@ -46,8 +53,8 @@ class CycleGAN():
         self.discriminator_iterations = 1  # Number of generator training iterations in each training loop
         self.beta_1 = 0.5
         self.beta_2 = 0.999
-        self.batch_size = 5
-        self.epochs = 100  # choose multiples of 25 since the models are save each 25th epoch
+        self.batch_size = 9
+        self.epochs = 25  # choose multiples of 25 since the models are save each 25th epoch
         self.save_interval = 1
         self.synthetic_pool_size = 25
 
@@ -645,6 +652,7 @@ class CycleGAN():
         return loss
 
     def truncateAndSave(self, real_, real, synthetic, reconstructed, path_name):
+
         if len(real.shape) > 3:
             real = real[0]
             synthetic = synthetic[0]
@@ -664,7 +672,10 @@ class CycleGAN():
         if self.channels == 1:
             image = image[:, :, 0]
 
-        toimage(image, cmin=0, cmax=1).save(path_name)
+        #toimage(image, cmin=0, cmax=1).save(path_name)
+        rescaled = (255.0 / (image.max() - image.min()) * (image - image.min())).astype(np.uint8)
+        im = Image.fromarray(rescaled)
+        im.save(path_name)
 
     def saveImages(self, epoch, real_image_A, real_image_B, num_saved_images=1):
         directory = os.path.join('images', self.date_time)

dose_estimator/load_data.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from PIL import Image
 from tensorflow.keras.utils import Sequence
+#from sklearn.preprocessing import MinMaxScaler
 #from skimage.io import imread
 
 
@@ -9,16 +10,22 @@ def load_data(nr_of_channels=1, batch_size=1, nr_A_train_imgs=None, nr_B_train_i
               nr_A_test_imgs=None, nr_B_test_imgs=None, subfolder='',
               generator=False, D_model=None, use_multiscale_discriminator=False, use_supervised_learning=False, REAL_LABEL=1.0):
     # load files
-    trainA_images = np.load('/home/peter/Documents/dose_estimator/data/pet_train.npy')
-    trainB_images = np.load('/home/peter/Documents/dose_estimator/data/ct_train.npy')
-    testA_images = np.load('/home/peter/Documents/dose_estimator/data/pet_test.npy')
-    testB_images = np.load('/home/peter/Documents/dose_estimator/data/ct_test.npy')
-    train_file = open("/home/peter/Documents/dose_estimator/data/train.txt", "r", encoding='utf8')
+    trainA_images = np.load('/home/peter/data/pet_train.npy') #np.load('/home/peter/Documents/dose_estimator/data/pet_train.npy')
+    trainB_images = np.load('/home/peter/data/ct_train.npy') #np.load('/home/peter/Documents/dose_estimator/data/ct_train.npy')
+    testA_images = np.load('/home/peter/data/pet_test.npy') #np.load('/home/peter/Documents/dose_estimator/data/pet_test.npy')
+    testB_images = np.load('/home/peter/data/ct_test.npy') #np.load('/home/peter/Documents/dose_estimator/data/ct_test.npy')
+    train_file = open("/home/peter/data/train.txt", "r", encoding='utf8')
     trainA_image_names = train_file.read().splitlines()
     trainB_image_names = trainA_image_names
-    test_file = open("/home/peter/Documents/data/test.txt", "r", encoding='utf8')
+    test_file = open("/home/peter/data/test.txt", "r", encoding='utf8')
     testA_image_names = test_file.read().splitlines()
     testB_image_names = testA_image_names
+
+    # normalize
+    trainA_images = normalize_array(trainA_images)
+    trainB_images = normalize_array(trainB_images)
+    testA_images = normalize_array(testA_images)
+    testB_images = normalize_array(testB_images)
 
     # add extra axis
     if nr_of_channels == 1:
@@ -27,17 +34,11 @@ def load_data(nr_of_channels=1, batch_size=1, nr_A_train_imgs=None, nr_B_train_i
         testA_images = testA_images[:, :, :, np.newaxis]
         testB_images = testB_images[:, :, :, np.newaxis]
 
-    # normalize
-    trainA_images = normalize_array(trainA_images)
-    trainB_images = normalize_array(trainB_images)
-    testA_images = normalize_array(testA_images)
-    testB_images = normalize_array(testB_images)
-
     # individually transform to 0 mean and std 1
-    trainA_images = convert_to_tf(trainA_images)
+    """trainA_images = convert_to_tf(trainA_images)
     trainB_images = convert_to_tf(trainB_images)
     testA_images = convert_to_tf(testA_images)
-    testB_images = convert_to_tf(testB_images)
+    testB_images = convert_to_tf(testB_images)"""
 
 
     return {"trainA_images": trainA_images, "trainB_images": trainB_images,
@@ -48,7 +49,7 @@ def load_data(nr_of_channels=1, batch_size=1, nr_A_train_imgs=None, nr_B_train_i
             "testB_image_names": testB_image_names}
 
     """trainA_path = os.path.join('data', subfolder, 'trainA')
-    
+
     trainB_path = os.path.join('data', subfolder, 'trainB')
     testA_path = os.path.join('data', subfolder, 'testA')
     testB_path = os.path.join('data', subfolder, 'testB')
@@ -97,13 +98,16 @@ def create_image_array(image_list, image_path, nr_of_channels):
 
 def convert_to_tf(array):
     return array
-  
-  
-  # If using 16 bit depth images, use the formula 'array = array / 32767.5 - 1' instead
-def normalize_array(array):
-    array = array / 127.5 - 1
-    return array
 
+
+  # If using 16 bit depth images, use the formula 'array = array / 32767.5 - 1' instead
+def normalize_array(inp, img_size=171):
+    array = inp.copy()
+    for i in range(array.shape[0]):
+        pic = array[i:(i+1),:,:].squeeze()
+        pic = 2*((pic - pic.min()) / (pic.max() - pic.min())) - 1 #pic / np.linalg.norm(pic) -1
+        array[i:(i+1),:,:] = pic
+    return array
 
 class data_sequence(Sequence):
 