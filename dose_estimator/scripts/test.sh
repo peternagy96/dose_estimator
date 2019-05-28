4f53fd0fb33d6d2e25df3a8d7e4e44a157a5d2a9
peternagy96 <peternagy96@gmail.com>
2019-05-28 17:31:40 +0200

    added test script

---
+++ dose_estimator/scripts/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source activate tf
+export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+python /home/peter/code/dose_estimator/CycleGAN.py /home/peter/first_model/ 100 test_jpg
