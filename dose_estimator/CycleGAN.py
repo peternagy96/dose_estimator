fix normalization for ct images

dose_estimator/CycleGAN.py
@@ -31,7 +31,7 @@ import cv2
 import keras.backend as K
 import tensorflow as tf
 import load_data
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 np.random.seed(seed=12345)
 
 
@@ -40,7 +40,7 @@ np.random.seed(seed=12345)
 
 
 class CycleGAN():
-    def __init__(self, model_path=None, load_epoch=None, mode='train', lr_D=2e-4, lr_G=3e-4, image_shape=(128, 128, 1), # orig: lr_G=3e-4
+    def __init__(self, model_path=None, load_epoch=None, mode='train', lr_D=3e-4, lr_G=3e-4, image_shape=(128, 128, 1), # orig: lr_G=3e-4
                  date_time_string_addition='', image_folder='MR'):
         self.img_shape = image_shape
         self.channels = self.img_shape[-1]

dose_estimator/load_data.py
@@ -12,9 +12,9 @@ def load_data(nr_of_channels=1, batch_size=1, nr_A_train_imgs=None, nr_B_train_i
               nr_A_test_imgs=None, nr_B_test_imgs=None, subfolder='',
               generator=False, D_model=None, use_multiscale_discriminator=False, use_supervised_learning=False, REAL_LABEL=1.0):
     # load files
-    trainA_images = np.load('/home/peter/data/numpy/pet_train.npy') #np.load('/home/peter/Documents/dose_estimator/data/pet_train.npy')
+    trainA_images = np.load('/home/peter/data/numpy/ct_train.npy') #np.load('/home/peter/Documents/dose_estimator/data/pet_train.npy')
     trainB_images = np.load('/home/peter/data/numpy/dose_train.npy') #np.load('/home/peter/Documents/dose_estimator/data/ct_train.npy')
-    testA_images = np.load('/home/peter/data/numpy/pet_test.npy') #np.load('/home/peter/Documents/dose_estimator/data/pet_test.npy')
+    testA_images = np.load('/home/peter/data/numpy/ct_test.npy') #np.load('/home/peter/Documents/dose_estimator/data/pet_test.npy')
     testB_images = np.load('/home/peter/data/numpy/dose_test.npy') #np.load('/home/peter/Documents/dose_estimator/data/ct_test.npy')
     train_file = open("/home/peter/data/numpy/train.txt", "r", encoding='utf8')
     trainA_image_names = train_file.read().splitlines()
@@ -121,9 +121,12 @@ def normalize_array(inp, img_size=81):
     array = inp.copy()
     for i in range(array.shape[0]):
         pic = array[i:(i+1),:,:]
+        pic = pic + pic.min()
         mask = (pic != 0.0)
-        pic[mask] = ((pic[mask] - pic.min()) / (pic.max() - pic.min()))  #pic / np.linalg.norm(pic) -1
+        pic[mask] = pic[mask] / pic.max()
+        #pic[mask] = ((pic[mask] - pic.min()) / (pic.max() - pic.min()))  # pic / np.linalg.norm(pic) -1 # 
         #pic[mask] = (pic[mask] - pic.mean()) / pic.std()
+        #pic[mask] = ((pic[mask] - pic.min()) / (pic.max() - pic.min()))
         array[i:(i+1),:,:] = pic
     return array
 