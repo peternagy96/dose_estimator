9e2fb9a9bc3c8014955d6332fcfe9bba821b1869
peternagy96 <peternagy96@gmail.com>
2019-06-13 17:59:08 +0200

    improved MIP

--- dose_estimator/scripts/test.sh
+++ dose_estimator/scripts/test.sh
@@ -4,8 +4,8 @@ source activate tf
 export PATH=/usr/local/cuda-9.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
-python /home/peter/code/dose_estimator/CycleGAN.py /home/peter/saved_models/jun12_PET/saved_models/ 100 test_jpg
-python /home/peter/code/dose_estimator/CycleGAN.py /home/peter/saved_models/jun12_PET/saved_models/ 200 test_jpg
+#python /home/peter/code/dose_estimator/CycleGAN.py /home/peter/saved_models/jun12_PET/saved_models/ 100 test_jpg
+#python /home/peter/code/dose_estimator/CycleGAN.py /home/peter/saved_models/jun12_PET/saved_models/ 200 test_jpg
 #python /home/peter/code/dose_estimator/CycleGAN.py /home/peter/saved_models/jun12_CTPET/saved_models/ 100 mip
 #python /home/peter/code/dose_estimator/CycleGAN.py /home/peter/saved_models/jun12_CTPET/saved_models/ 200 mip
-#python /home/peter/Documents/dose_estimator-git/dose_estimator/CycleGAN.py /home/peter/Documents/dose_estimator-git/results/jun10_petct-fullfilter1/saved_models/ 200 mip
+python /home/peter/Documents/dose_estimator-git/dose_estimator/CycleGAN.py /home/peter/Documents/results/jun12_corrected/20190612-150913_CTPET/saved_models/ 200 mip
