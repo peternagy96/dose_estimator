5c422fc0b9df125cd8d8e2aef92e0362dd7be3c9
Peter Nagy <peternagy96@gmail.com>
2019-06-18 16:38:57 +0200

    added settings file

--- dose_estimator/dose_estimator.py
+++ dose_estimator/dose_estimator.py
@@ -3,6 +3,10 @@ from models.gan import cycleGAN
 #from trainer import Trainer
 
 if __name__ == '__main__':
+    # import settings from file
+
+    # import data
+
     # import model
     gan = cycleGAN(result_name='test', mode_G='basic', mode_D='basic', model_path=None,
                    lr_D=3e-4, lr_G=3e-4, image_shape=(128, 128, 2))
